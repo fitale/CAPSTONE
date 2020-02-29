c01f3bf04999a0f386e439b15881098f8ec0c5bd
fitale <fitalewari@gmail.com>
2020-02-28 16:31:47 -0800

    header-mobile sort of styled, folders created

---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <h3 className="header__title">upNEXT</h3>
+        <nav className="header__nav">
+          <h6 className="header__nav--item-one">SIGN IN</h6>
+          <h6 className="header__nav--item">SIGN UP</h6>
+        </nav>
+      </header>
+    </>
+  );
+}
