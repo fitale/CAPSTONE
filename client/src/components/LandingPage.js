b333bd35acd731c93150f6e343fc1fc32d08f740
fitale <fitalewari@gmail.com>
2020-03-19 11:54:14 -0700

    landing-page.scss complete

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 import Hero from "./Hero";
 import Footer from "./Footer";
 
+import movie from "../assets/videos/BrainStation.mp4";
+import study from "../assets/images/study.jpg";
+import saveMoney from "../assets/images/save-money.jpg";
 export default class LandingPage extends Component {
   render() {
     return (
@@ -18,16 +21,46 @@ export default class LandingPage extends Component {
             Tell us about yourself and we'll provide you with a list of
             universities we think you ought to check out.
           </h5>
-
-          <Link to="/uChoosenow" className="landing-page__button">
-            <h5 className="landing-page__button--text">uChoose now</h5>
-          </Link>
-          <div className="landing-page__iframe">
-            <iframe
-              title="iframe"
-              className="landing-page__iframe--map"
-              src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA_JOFNWwvsXhy3dsmSRX7nqi9KQy5Db7U&q=NYS"
-            ></iframe>
+          <div className="landing-page__major">
+            <div className="landing-page__major--flex">
+              <Link to="/uChoosenow" className="button">
+                <h5 className="button__text">uChoose now</h5>
+              </Link>
+              <h3 className="smaller">
+                SPEND LESS TIME SEARCHING, AND MORE TIME MAKING IT HAPPEN
+              </h3>
+            </div>
+            <div className="landing-page__major--flex-vid">
+              <video controls src={movie} className="video"></video>
+            </div>
+          </div>
+          <div className="landing-page__holder">
+            <div className="landing-page__holder--left">
+              <h3 className="bold-header">
+                Discover which universities match your price range
+              </h3>
+              <h5 className="para">
+                We'll help you save money on university by finding schools that
+                match your preferred tuition's net price.
+              </h5>
+            </div>
+            <div className="landing-page__holder--right">
+              <img src={saveMoney} alt="save-money" className="img" />
+            </div>
+          </div>
+          <div className="landing-page__holder">
+            <div className="landing-page__holder--left">
+              <h3 className="bold-header">
+                Learn more about the admission process
+              </h3>
+              <h5 className="para">
+                We'll compare your chance of admission with other previously
+                accepted students and help you achieve your goals.
+              </h5>
+            </div>
+            <div className="landing-page__holder--right">
+              <img src={study} alt="planning" className="img" />
+            </div>
           </div>
           <Footer />
         </main>
--- client/src/components/Locations.js
+++ client/src/components/Locations.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 import GoogleMapReact from "google-map-react";
 
 // import location from "../assets/icons/location.svg";
@@ -18,49 +18,78 @@ export default class Locations extends Component {
     zoom: 6
   };
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   console.log("reached componentDidUpdate");
-  //   console.log(`this is prevProps: ${JSON.stringify(prevProps)}`);
-  //   console.log(`this is prevState: ${JSON.stringify(prevState)}`);
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    // console.log("reached componentDidUpdate");
+    // console.log(`this is prevProps: ${JSON.stringify(prevProps)}`);
+    // console.log(`this is prevState: ${JSON.stringify(prevState)}`);
+  }
 
   searchByLocation = event => {
     event.preventDefault();
     // console.log(event.target.input.value);
     let location = event.target.input.value;
-    // OG way
-    axios.get("http://localhost:5000/api/udata").then(res => {
-      const buffUnis = res.data.filter(uni =>
-        uni.city.toLowerCase().includes(location.toLowerCase())
-      );
-      console.log(buffUnis);
-      const newCenter = {};
-      newCenter.lat = buffUnis[0].lat;
-      newCenter.lng = buffUnis[0].lng;
-      const myMarker = buffUnis.map(oneUni => {
-        console.log(`latitude of one university in buffalo: ${oneUni.lat}`);
-        console.log(`longitude of one university in buffalo: ${oneUni.lng}`);
-        return (
-          <AnyReactComponent
-            lat={oneUni.lat}
-            lng={oneUni.lng}
-            text="here i am"
-            className="locations__map-holder--icon"
-          />
-        );
-      });
-      this.setState(
-        {
-          city: location,
-          center: newCenter,
-          marker: myMarker,
-          zoom: 11
-        },
-        () => {
-          console.log(this.state);
-        }
+    const mapUni = this.props.uData.filter(uni => {
+      return uni.city.toLowerCase().includes(location.toLowerCase());
+    });
+    // console.log(mapUni);
+    const newCenter = {};
+    newCenter.lat = mapUni[0].lat;
+    newCenter.lng = mapUni[0].lng;
+    const myMarker = mapUni.map(oneUni => {
+      console.log(`latitude of one university in buffalo: ${oneUni.lat}`);
+      console.log(`longitude of one university in buffalo: ${oneUni.lng}`);
+      return (
+        <AnyReactComponent
+          lat={oneUni.lat}
+          lng={oneUni.lng}
+          text="here i am"
+          className="locations__map-holder--icon"
+        />
       );
     });
+    this.setState(
+      {
+        city: location,
+        center: newCenter,
+        marker: myMarker,
+        zoom: 2
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
+    // axios.get("http://localhost:5000/api/udata").then(res => {
+    //   const buffUnis = res.data.filter(uni =>
+    //     uni.city.toLowerCase().includes(location.toLowerCase())
+    //   );
+    // console.log(buffUnis);
+    // const newCenter = {};
+    // newCenter.lat = buffUnis[0].lat;
+    // newCenter.lng = buffUnis[0].lng;
+    // const myMarker = buffUnis.map(oneUni => {
+    //   console.log(`latitude of one university in buffalo: ${oneUni.lat}`);
+    //   console.log(`longitude of one university in buffalo: ${oneUni.lng}`);
+    //   return (
+    //     <AnyReactComponent
+    //       lat={oneUni.lat}
+    //       lng={oneUni.lng}
+    //       text="here i am"
+    //       className="locations__map-holder--icon"
+    //     />
+    //   );
+    // });
+    // this.setState(
+    //   {
+    //     city: location,
+    //     center: newCenter,
+    //     marker: myMarker,
+    //     zoom: 11
+    //   },
+    //   () => {
+    //     console.log(this.state);
+    //   }
+    // );
+    // });
   };
 
   render() {
