ef481fd0709e2ee1bf6f6a152e9510827a23f968
fitale <fitalewari@gmail.com>
2020-03-23 17:11:24 -0700

    data and about pages done

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,36 @@
 import React, { Component } from "react";
+import headshot from "../assets/images/headshot.jpg";
 
 export default class About extends Component {
   render() {
-    return <div>dis is da about page</div>;
+    return (
+      <main className="main">
+        <h1 className="main__welcome">about uChoose</h1>
+        <h2 className="main__name">by fitale wari</h2>
+        <section className="main__section">
+          <img
+            className="main__section--headshot"
+            src={headshot}
+            alt="fitale-headshot"
+          />
+          <div className="main__section--div">
+            <h3 className="label">
+              uChoose is more than just a university search tool. It is a new
+              way to make smarter decisions when it comes to planning for
+              university.
+            </h3>
+            <h3 className="para">
+              uChoose suggests universities students ought to consider based on
+              simplified campus match scores, tuition costs, and location
+              matches. Not only can they find the schools that best fit their
+              academic goals, but they can find institutions that are most
+              affordable and begin planning to apply. With uChoose, students and
+              families have the power to begin their univeristy search process
+              and make the best decisions.
+            </h3>
+          </div>
+        </section>
+      </main>
+    );
   }
 }
--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -1,7 +1,53 @@
 import React, { Component } from "react";
 
+import IPEDS from "../assets/images/IPEDS.png";
+import nygov from "../assets/images/nygov.png";
+import myAPI from "../assets/images/myAPI.png";
 export default class Data extends Component {
   render() {
-    return <div>here's the data i used</div>;
+    return (
+      <main className="main-data">
+        <h1 className="main-data__h1">DATA</h1>
+        <div className="main-data__div">
+          <div className="main-data__div--flex">
+            <a
+              className="a"
+              href="https://nces.ed.gov/ipeds/datacenter/InstitutionByName.aspx"
+            >
+              <img className="img" src={IPEDS} alt="site-screenshot" />
+            </a>
+            <h3 className="h3">
+              IPEDS is the Institute of Education Sciences (IES), the US’s
+              leading source for rigorous, independent education research,
+              evaluation and statistics.
+            </h3>
+          </div>
+          <div className="main-data__div--flex">
+            <a
+              className="a"
+              href="https://data.ny.gov/api/views/3cij-nwhw/rows.json?accessType=DOWNLOAD&sorting=true"
+            >
+              <img className="img" src={nygov} alt="site-screenshot" />
+            </a>
+            <h3 className="h3">
+              Consulted data.ny.gov to build uChoose's API. data.ny.gov provides
+              highlights on undergraduate and graduate enrollment, as well as
+              some program area offerings for schools registered in the State
+              University of New York (SUNY) System.
+            </h3>
+          </div>
+          <div className="main-data__div--flex">
+            <a className="a" href="http://localhost:5000/api/udata">
+              <img className="img" src={myAPI} alt="site-screenshot" />
+            </a>
+            <h3 className="h3">
+              Built the API that uChoose uses to teach users about schools.
+              Highlights information about campus overview and provides insight
+              to admission statistics.
+            </h3>
+          </div>
+        </div>
+      </main>
+    );
   }
 }
