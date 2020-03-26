fbfaf3fd809a385f40070b22c7fb9d14c5122070
fitale <fitalewari@gmail.com>
2020-03-26 09:49:22 -0700

    created news page using two apis. some news articles aren't related, so must fix at a later date. responsive design complete

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,7 +1,79 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const newsAPI_KEY = "924908deb4f7425a85d441b973795506";
+const gNews_API_KEY = "33879167-2ec2-4971-997b-80ee8f9ef01a";
 
 export default class News extends Component {
+  state = {
+    gNews: [],
+    newsAPI: []
+  };
+  componentDidMount() {
+    axios
+      .get(
+        `https://content.guardianapis.com/search?q=higher%20education%20AND%20education&api-key=${gNews_API_KEY}`
+      )
+      .then(res => {
+        axios
+          .get(
+            `http://newsapi.org/v2/everything?q=education&from=2020-03-01&sortBy=popularity&apiKey=${newsAPI_KEY}`
+          )
+          .then(res2 => {
+            console.log(
+              "res:",
+              res.data.response.results,
+              "res2: ",
+              res2.data.articles
+            );
+            this.setState({
+              gNews: res.data.response.results,
+              newsAPI: res2.data.articles
+            });
+          });
+      }, console.log(this.state))
+      .catch(err => {
+        console.log(err);
+      });
+  }
   render() {
-    return <div>newszzzzzzz</div>;
+    if (this.state.gNews === 0 || this.state.newsAPI === 0) {
+      return <h5>loading...</h5>;
+    } else {
+      const guardianStories = this.state.gNews.map(story => {
+        return (
+          <div className="news__section--div" key={story.id}>
+            <a className="url" href={`${story.webUrl}`}>
+              <h5 className="url__h5">{story.webTitle}</h5>
+              <p className="url__description">{story.webPublicationDate}</p>
+            </a>
+          </div>
+        );
+      });
+      const newsAPIStories = this.state.newsAPI.map(article => {
+        return (
+          <div className="news__section--div" key={article.source.id}>
+            <a href={`${article.url}`} className="url">
+              <h5 className="url__h5">{article.title}</h5>
+              <img
+                className="url__img"
+                src={`${article.urlToImage}`}
+                alt="article-cover-photo"
+              />
+              <p className="url__description">{article.description}</p>
+            </a>
+          </div>
+        );
+      });
+      return (
+        <main className="news">
+          <h1 className="news__title">Articles related to higher education</h1>
+          <section className="news__section">
+            {newsAPIStories}
+            {guardianStories}
+          </section>
+        </main>
+      );
+    }
   }
 }
